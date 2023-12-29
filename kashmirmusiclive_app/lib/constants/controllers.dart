58b768170d4730b66eeff5663e5a7dd465e4383b
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2023-12-29 17:09:16 +0530

    updated tech stack
    Signed-off-by: Muhammad Tabish Khanday <mtabishkhanday@gmail.com>

--- kashmirmusiclive_app/lib/constants/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:kashmirmusiclive_app/controllers/menu_controller.dart';
-import 'package:kashmirmusiclive_app/controllers/navigation_controller.dart';
-
-MenuController menuController = MenuController.instance;
-NavigationController navigationController = NavigationController.instance;
--- kashmirmusiclive_app/lib/constants/style.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color light = Color(0xFFF7F8FC);
-Color lightGrey = Color(0xFFA4A6B3);
-Color dark = Color(0xFF000000);
-Color active = Color(0xFFED1B24);
