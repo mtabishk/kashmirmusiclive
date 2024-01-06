40bc81b07bd89e30b5fa8bbff1d7751d26182127
Muhammad Tabish Khanday <mtabishkhanday@gmail.com>
2024-01-06 23:27:39 +0530

    module navbar path
    Signed-off-by: Muhammad Tabish Khanday <mtabishkhanday@gmail.com>

--- kashmirmusiclive/app/(public)/layout.tsx
+++ kashmirmusiclive/app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 import { Footer } from "@/components/footer";
 import { MobileNavBar } from "@/components/mobile-navbar";
-import { Navbar } from "@/components/navbar";
+import { Navbar } from "../../components/navbar";
 import React from "react";
 
 export default function PublicLayout({
